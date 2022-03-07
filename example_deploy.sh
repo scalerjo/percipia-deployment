318fd4c4edcf0a15a52b5cbb6061c499e0c73e1a
scalerjo <joseph.scalera1@gmail.com>
2022-03-07 17:10:09 +0000

    add to .gitignore, and generate nginx config based on input host ipv4

--- example_deploy.sh
+++ example_deploy.sh
@@ -2,7 +2,8 @@
 
 # You will need to update this to your machines IPv4 Address.
 # This is because the backend server can't use localhost when in a docker container.
-# It's possible to use Docker networks instead, but this is fine for the sake of the demonstration.
+# It's possible to use Docker networks instead, but I don't want to mess with your docker configuration
+# So this will work for the sake of the demonstration.
 HOST=192.168.200.2
 
 # Deploy Database
@@ -15,7 +16,10 @@ cd backend
 ./start.sh $HOST
 cd ..
 
+# This will take a few minutes since it has to build the react project.
+# This is where I would do SSL, but since this is a demonstration, the app will run on port 80 over http
+# The host field needs to be passed for the same reason as before. NGINX will route traffic to the backend docker container for API calls
 cd nginx
-./build.sh
+./build.sh $HOST
 ./start.sh
 cd ..
--- nginx/build.sh
+++ nginx/build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# git clone https://github.com/scalerjo/percipia-frontend.git
-# cd percipia-frontend
-# npm install
-# npm run build
-# mv build/ ../public
-# cd ..
-#rm -r percipia-frontend
+git clone https://github.com/scalerjo/percipia-frontend.git
+cd percipia-frontend
+npm install
+npm run build
+mv build/ ../public
+cd ..
+rm -r percipia-frontend
+
+./genNginxConf.sh $1
 
 docker build -t nginx .
-#rm -r public
+rm -r public
+rm default.conf
